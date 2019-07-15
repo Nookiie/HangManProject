19ae9ca9b0e58d3e85b4de6e1a6f9b5a02f72b65
Viktor Ivanov <33068739+Nookiie@users.noreply.github.com>
2019-07-15 14:32:09 +0300

    ClientSide Blazor Initialized

---
+++ Data/Entities/GameItems/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using HM.Data.Abstraction;
+using HM.Data.Entities;
+
+namespace HM.Data.Entities.GameItems
+{
+    /// <summary>
+    /// The Main Game Manager that coordinates and manages the wordlist
+    /// </summary>
+    public class Category : BaseEntity<int>
+    {
+        #region Constructor
+
+        public Category()
+        {
+
+        }
+
+        public Category(string category)
+        {
+            this.Name = category;
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Category Name is too short")]
+        [MaxLength(40, ErrorMessage = "Category Name is too long")]
+        public string Name { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ HM.Blazor.Client/Components/WordsManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HM.Blazor.Client.Components
+{
+    public class WordsManager : ComponentBase
+    {
+
+    }
+}
