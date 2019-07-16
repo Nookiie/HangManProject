25914df2b1d5abf62544d35aef7177c603cf78e5
Viktor Ivanov <33068739+Nookiie@users.noreply.github.com>
2019-07-16 17:58:00 +0300

    Additions to Test Class + Razor Logout Page

---
+++ HangmanTest/Game/LogicTest.cs
@@ -0,0 +1,214 @@
+using HM.Data.Entities.GameItems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using HM.Data.Entities.Difficulty;
+using HM.Data.Abstraction;
+using HangmanLogic.Logic;
+using HM.Data.Entities.GameCondition;
+
+namespace HangmanTest
+{
+    [TestClass]
+    public static class LogicTest
+    {
+        [TestMethod]
+        [TestCategory("Argument")]
+        [DataRow('a')]
+        [DataRow('s')]
+        [DataRow('f')]
+        [DataRow('1')]
+        [DataRow('A')]
+        public static void InputGuessTest(char input)
+        {
+            if (char.IsWhiteSpace(input))
+            {
+                throw new ArgumentException("Input Symbol is whitespace: " + input.ToString());
+            }
+
+            if (char.IsUpper(input))
+            {
+                throw new ArgumentException("Input Symbol is upper-case: " + input.ToString());
+            }
+
+            if (char.IsNumber(input) || char.IsDigit(input))
+            {
+                throw new ArgumentException("Input symbol is a number: " + input.ToString());
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Argument")]
+        public static void GameConditionArgumentTest(GameCondition gameCondition)
+        {
+            switch (gameCondition)
+            {
+                case GameCondition.Lost: return;
+                case GameCondition.Won:  return;
+                default: throw new ArgumentException("Unknown game condition");
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Argument")]
+        public static void GameDifficultyArgumentTest(GameDifficulty gameDifficulty)
+        {
+            switch (gameDifficulty)
+            {
+                case GameDifficulty.Easy: return; 
+                case GameDifficulty.Normal: return; 
+                case GameDifficulty.Hard: return; 
+                case GameDifficulty.Insane: return; 
+                default: throw new ArgumentException("Unknown game difficulty: " + gameDifficulty.ToString());
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Cleanup")]
+        public static void CleanupTest()
+        {
+            IGameData gameTracker = new GameData();
+            gameTracker.CleanUp();
+
+            int expectedValue = 0;
+
+            Assert.AreEqual(expectedValue, gameTracker.GetTries());
+            Assert.AreEqual(expectedValue, gameTracker.GetScore());
+            Assert.AreEqual(expectedValue, gameTracker.GetAttemptCount());
+        }
+
+        [TestMethod]
+        [TestCategory("Argument")]
+        public static void GameDataArgumentTest(GameData gameData)
+        {
+            Assert.IsNotNull(gameData);
+            
+            if (gameData.Words.Count == 0)
+            {
+                throw new ArgumentException("List of words is empty:" + gameData.ToString());
+            }
+            
+            foreach (var word in gameData.Words)
+            {
+                if (string.IsNullOrWhiteSpace(word.Name))
+                {
+                    throw new ArgumentException("Word name contains whitespace or has none: " + word.ToString());
+                }
+                foreach (var letter in word.Name)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        throw new ArgumentException("Word name has upper-case letters: " + word.ToString());
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public static void DifficultyScoreTest(GameDifficulty difficulty)
+        {
+            Assert.IsNotNull(difficulty);
+
+            IGameData gameTracker = new GameData();
+
+            var expectedScoreOnWinEasy = 10;
+            var expectedScoreOnWinNormal = 15;
+            var expectedScoreOnWinHard = 20;
+            var expectedScoreOnWinInsane = 25;
+
+            switch (difficulty)
+            {
+                case GameDifficulty.Easy: Assert.AreEqual(expectedScoreOnWinEasy, gameTracker.GetScore()); break;
+                case GameDifficulty.Normal: Assert.AreEqual(expectedScoreOnWinNormal, gameTracker.GetScore()); break;
+                case GameDifficulty.Hard: Assert.AreEqual(expectedScoreOnWinHard, gameTracker.GetScore()); break;
+                case GameDifficulty.Insane: Assert.AreEqual(expectedScoreOnWinInsane, gameTracker.GetScore()); break;
+                default: throw new ArgumentException("Unknown game difficulty: " + difficulty.ToString());
+            }
+        }
+
+        [TestMethod]
+        public static void GameTrackerFailTest()
+        {
+            IGameData gameTracker = new GameData();
+
+            var expectedAttempts = 1;
+            var expectedTriesOnFail = 8;
+            var expectedScoreOnFail = 0;
+
+            gameTracker.Fail();
+
+            Assert.AreEqual(expectedAttempts, gameTracker.GetAttemptCount());
+            Assert.AreEqual(expectedTriesOnFail, gameTracker.GetTries());
+            Assert.AreEqual(expectedScoreOnFail, gameTracker.GetScore());
+        }
+
+        [TestMethod]
+        public static void GameTrackerWinTest()
+        {
+            IGameData gameTracker = new GameData();
+            Word chosenWord = new Word();
+
+            var expectedAttempts = 1;
+            var expectedTriesOnWin = 9;
+
+            gameTracker.Win(chosenWord);
+
+            Assert.AreEqual(expectedAttempts, gameTracker.GetAttemptCount());
+            Assert.AreEqual(expectedTriesOnWin, gameTracker.GetTries());
+        }
+
+        [TestMethod]
+        public static void GameOverWinEffectOnTriesTest(Word word)
+        {
+            IGameData gameTracker = new GameData();
+            var expectedTriesOnWin = 9;
+
+            gameTracker.Win(word);
+
+            Assert.AreEqual(expectedTriesOnWin, gameTracker.GetTries());
+        }
+
+        [TestMethod]
+        public static void GetRandomLetterTest()
+        {
+            List<Word> words = new List<Word>
+            {
+                new Word("elephant"),
+            };
+            GameData gameData = new GameData();
+
+            gameData.Words = words;
+
+            Word chosenWord = gameData.GetRandomWord();
+            Word printWord = chosenWord;
+            char input = GameLogic.GetJoker(gameData, chosenWord, printWord);
+
+            if (!chosenWord.Name.Contains(input))
+            {
+                throw new ApplicationException("Letter is not part of the chosen word: " + input.ToString());
+            }
+        }
+
+        [TestMethod]
+        public static void GetRandomLetterMinPriorityTest()
+        {
+            List<Word> words = new List<Word>
+            {
+                new Word("elephant"),
+            };
+
+            var unexpectedLetter = 'e';
+
+            GameData gameData = new GameData();
+
+            gameData.Words = words;
+
+            Word chosenWord = gameData.GetRandomWord();
+            Word printWord = chosenWord;
+            char input = GameLogic.GetJoker(gameData, chosenWord, printWord);
+
+            Assert.AreNotEqual(unexpectedLetter, input);
+        }
+    }
+}
+
